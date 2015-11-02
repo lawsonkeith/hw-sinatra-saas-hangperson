5d7ab29097e7db2982ff1ebc1f4cab49d0a6eddc
Keith Lawson <keithl.online@gmail.com>
2015-11-02 17:37:25 +0000

    class mods

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -12,11 +12,21 @@ class HangpersonGame
     @word = word
   end
 
+  # CLASS method -ref with class name
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def SpillBeans
+    puts @word
+  end
 
 end
+
+
+RandWrd = HangpersonGame.get_random_word
+bob = HangpersonGame.new(RandWrd)
+bob.SpillBeans
